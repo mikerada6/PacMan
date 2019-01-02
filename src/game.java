b135db219a7868460831a876f1958f317a3484c9
Michael Radaszkiewicz <mrada6@gmail.com>
2019-01-01 20:31:12 -0500

    JFrame set up

---
+++ src/game.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class game {
+    public static void main(String[] args)
+    {
+        JFrame window = new JFrame("Rezatron Pac-Man");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.pack();
+        window.setVisible(true);
+
+    }
+}
